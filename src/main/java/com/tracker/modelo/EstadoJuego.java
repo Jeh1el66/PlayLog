d02e48d46dec8d97ebb544138f0499b4792bba0e
Jeh1el66 <zs21027321@estudiantes.uv.mx>
2026-04-27 20:06:19 -0600

    se creo el enum para usar en la bibliotecas del usuario, para saber el estado del juego que pondra

---
+++ src/main/java/com/tracker/modelo/EstadoJuego.java
@@ -0,0 +1,11 @@
+package com.tracker.modelo;
+
+
+ //posibles estados de un juego en la biblioteca del usuario
+
+public enum EstadoJuego {
+    JUGANDO,
+    COMPLETADO,
+    QUIERO_JUGAR,
+    ABANDONADO
+}
